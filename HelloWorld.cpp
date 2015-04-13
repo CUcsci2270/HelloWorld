4f7d831ddf28b063b41029e84554e77147e05cec
brkn8102 <brkn8102@colorado.edu>
2015-04-13 16:41:24 -0600

    added a comment

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
 int main(int argc, char*argv[])
 {
 	cout << "Hello World!!" << endl;
 	return 0;
+
+	// I made this change.
 }
