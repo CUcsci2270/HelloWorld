59d4b2f1bf367e31ef5899c1b0d32abdaa0a489c
Chia-hsin Yang <chya5202@colorado.edu>
2015-04-14 15:26:07 -0600

    It workedgit add C_Yang.cpp

---
+++ C_Yang.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char*argv[] ) {
+  cout <<"Hello, everyone!">> endl;
+    return 0;
+}
--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,11 @@
+//Chia-hsin Yang
+
 #include <iostream>
 
 using namespace std; 
 
 int main(int argc, char*argv[])
 {
-	cout << "Hello!" << endl;
+	cout << "Hello everyone!" << endl;
 	return 0;
 }
