80aeaadc22af7325d0174a0ed5723ba5c0ba019e
ComputerBear <sean.harrison@colorado.edu>
2015-04-14 14:59:03 -0600

    AMoreExcitedHelloWorld

---
+++ Sean_Harrison.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char*argv[])
+{
+	cout << "Hello World!!!!!!!!!!!!!!!!!!!!!!!!!" << endl;
+	cout<< "Today is going to be a good day, so don't forget to smile."<<endl;
+	return 0;
+}
