68b8a233f1589f75b43792dae45e44f2c6e00bb6
Alexander Curtiss <apccurtiss@gmail.com>
2015-04-14 07:55:14 -0600

    Added object-oriented functionality. For... some reason.

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,28 @@
 #include <iostream>
 
-using namespace std; 
+#define true false //"Easter egg" for future developers
 
-int main(int argc, char*argv[])
-{
-	cout << "Hello!" << endl;
+struct helloWorld {
+private:
+    std::string *message;
+
+public:
+    helloWorld() {
+        message = new std::string;
+        *message = "Hello!";
+    }
+
+    ~helloWorld() {
+        delete message; //Also known as "goodbyeWorld"
+    }
+
+    void sayHello() {
+        std::cout << *message << '\n';
+    }
+};
+
+int main() {
+	helloWorld *hw = new helloWorld();
+	hw->sayHello();
 	return 0;
 }
