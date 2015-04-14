2835221632f7fb158437851dbb0590b0359c00a8
Rebecca Robb <robbr@colorado.edu>
2015-04-14 14:51:52 -0600

    added file for recit 10

--- Rebecca_Robb.cpp
+++ Rebecca_Robb.cpp
@@ -4,6 +4,8 @@ using namespace std;
 
 int main(int argc, char*argv[])
 {
-	cout << "Hello" << endl;
+    cout << "Beware that, when fighting monsters, you yourself do not become a monster..." << endl;
+    cout << "For when you gaze long into The Abyss, The Abyss gazes also into you." << endl;
 	return 0;
 }
+
