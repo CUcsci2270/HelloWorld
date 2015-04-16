91faab1529116449dd07fb403fc06ce95d2a5cff
Hayden Reich <hayden.reich@colorado.edu>
2015-04-16 08:24:28 -0600

    added cout statement to HelloWorld.cpp

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
 int main(int argc, char*argv[])
 {
 	cout << "Hello!" << endl;
+	cout<< "Changes made, goodbye!"<<endl;
 	return 0;
 }
