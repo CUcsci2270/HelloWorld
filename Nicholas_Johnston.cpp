a5b506a2d09ea500738008edcbaa41774410857c
Nicholas Johnston <nijo5747@colorado.edu>
2015-04-16 16:14:18 -0600

    Added hello world program for Nicholas Johnston

---
+++ Nicholas_Johnston.cpp
@@ -0,0 +1,13 @@
+//Nicholas Johnston
+//Addition to HelloWorld repository for Recitation 10
+//nijo5747
+
+#include <iostream>
+
+using namespace std; 
+
+int main(int argc, char*argv[])
+{
+	cout << "Hello World!!" << endl;
+	return 0;
+}
