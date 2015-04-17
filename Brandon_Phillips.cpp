38c12755169d74303f8e76f87b192de47bd004b1
Brandon Phillips <bericp1@gmail.com>
2015-04-16 20:34:56 -0600

    Added Brandon_Phillips.cpp and a message

---
+++ Brandon_Phillips.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std; 
+
+int main(int argc, char*argv[])
+{
+	cout << "Ich heisse Brandon! Hier haben viele Studenten Dateien aber meine ist die einzige, die auf Deutsch ist." << endl;
+	return 0;
+}
