403e50c5e86f0a25bd8651810edb78152597be05
Kathryn Gray <shiny@engr2-1-20-159-edu.int.colorado.edu>
2016-04-12 17:43:03 -0600

    Added a cout statement

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,11 @@
+//edited 4/12/16 by Kathryn Gray
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
 int main(int argc, char*argv[])
 {
 	cout << "Hello!" << endl;
+	cout << "Hello it's me" << endl;
 	return 0;
 }
