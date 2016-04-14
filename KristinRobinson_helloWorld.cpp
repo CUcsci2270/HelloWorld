7899b92ff7229deef9d28c0d564516a53b236e8a
RobinsonK <Kristin.Robinson-1@gmail.com>
2016-04-14 11:49:03 -0600

    test message

---
+++ KristinRobinson_helloWorld.cpp
@@ -0,0 +1,18 @@
+//Kristin Robinson
+//CSCI 2270 - 315
+//Hoenigman
+//Amber Womack
+//Recitation 9
+//Test file for Project 1
+
+//Basic hello world program
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    cout << "Hello world!" << endl;
+    return 0;
+}
