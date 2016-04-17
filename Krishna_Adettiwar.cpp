c15e1240f20bf389d3cb485795ff6ea1db9ee091
kri111 <krishna.adettiwar@colorado.edu>
2016-04-17 12:13:23 -0600

    Update Krishna_Adettiwar.cpp

--- Krishna_Adettiwar.cpp
+++ Krishna_Adettiwar.cpp
@@ -7,3 +7,9 @@ int main()
     cout << "Hello world! I am Krishna Adettiwar." << endl;
     return 0;
 }
+
+//Krishna Adettiwar
+//krishna.adettiwar@colorado.edu
+//Working with Tanay Choudhary
+//Github username: kri111
+//link to test repository: https://github.com/kri111/Adettiwar_Choudhary_CSCI2270_FinalProject.git
