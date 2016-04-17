c5ed8ad36d444f3d9ad2a486afb6b2280ce3914a
moltencolton <colton.chambers@colorado.edu>
2016-04-17 15:47:47 -0600

    added This is colton to cout statement

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 
-using namespace std; 
+using namespace std;
 
 int main(int argc, char*argv[])
 {
-	cout << "Hello!" << endl;
+	cout << "Hello! This is Colton." << endl;
 	return 0;
 }
