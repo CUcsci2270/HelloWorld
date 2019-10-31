4d5fac7fc8fd145d31daf2496f653d8165afa9c5
divakarpuro <44550134+divakarpuro@users.noreply.github.com>
2019-10-31 20:05:24 +0530

    Updated
    // Simple C++ program to display "Hello World"

--- Luke_Worley.cpp
+++ Luke_Worley.cpp
@@ -1,9 +1,16 @@
-#include <iostream>
-
+// Simple C++ program to display "Hello World" 
+  
+// Header file for input output functions 
+#include<iostream>  
+  
 using namespace std; 
-
-int main(int argc, char*argv[])
-{
-	cout << "Hello World!!" << endl;
-	return 0;
-}
+  
+// main function - 
+// where the execution of program begins 
+int main() 
+{ 
+    // prints hello world 
+    cout<<"Hello World"; 
+      
+    return 0; 
+} 
